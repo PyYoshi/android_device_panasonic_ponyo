9a17c5225d3da61a6259a8312afd33a9822813e6
devrenax <cm4is01_devrenax@yahoo.co.jp>
2012-10-10 00:46:04 +0900

    コメントアウトミスの修正 いくつか試験的にオプションを追加修正

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,18 +3,16 @@
 
 # Call headers from msm-3.0: needed to build libs in hardware/qcom/display
 # not work? build/core/binary.mk(489:492)にTARGET_SPECIFIC_HEADER_PATHの定義はされている
-LOCAL_PATH := $(call my-dir)
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
-append LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+TARGET_SPECIFIC_HEADER_PATH := device/panasonic/ponyo/include
 
-USE_CAMERA_STUB := true
+#USE_CAMERA_STUB := true
 
 TARGET_BOOTLOADER_BOARD_NAME := ponyo
 
 TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := msm7x30
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
-TARGET_USES_2G_VM_SPLIT := true
+#TARGET_USES_2G_VM_SPLIT := true
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -29,12 +27,17 @@ BOARD_VENDOR_QCOM_AMSS_VERSION := 50000
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 BOARD_EGL_CFG := device/panasonic/ponyo/prebuilt/egl.cfg
-BOARD_USE_FRAMEBUFFER_ALPHA_CHANNEL := true
+#BOARD_USE_FRAMEBUFFER_ALPHA_CHANNEL := true
 BOARD_USE_SCREENCAP := true
-BOARD_OVERLAY_MINIFICATION_LIMIT := 2
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE # http://forum.xda-developers.com/showpost.php?p=24228157&postcount=301
+#BOARD_OVERLAY_MINIFICATION_LIMIT := 2
+#TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+#TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+#BOARD_OVERLAY_FORMAT_YCbCr_420_SP := true
+#TARGET_USES_SF_BYPASS := true
+TARGET_GRALLOC_USES_ASHMEM :=true
+TARGET_USES_OVERLAY := true
+USE_OPENGL_RENDERER := true
 
 #
 BOARD_CUSTOM_GRAPHICS := ../../../device/panasonic/ponyo/recovery/graphics.c
@@ -58,12 +61,12 @@ WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_P2P     := "/vendor/firmware/fw_bcmdhd_p2p.bin"
 
-BOARD_KERNEL_CMDLINE := console=ttyMSM1 androidboot.hardware=qcom androidboot.baseband=msm
+#BOARD_KERNEL_CMDLINE := console=ttyMSM1 androidboot.hardware=qcom androidboot.baseband=msm
 BOARD_KERNEL_BASE := 0x13800000
 BOARD_KERNEL_PAGESIZE := 4096
 
 # fix this up by examining /proc/mtd on a running device
-dev:    size   erasesize  name
+#dev:    size   erasesize  name
 #mtd0: 00a00000 00040000 "boot"
 #mtd1: 15e00000 00040000 "system"
 #mtd2: 0c800000 00040000 "cache"
@@ -94,6 +97,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 # HEX(0x00040000) = DEC(262144)
 BOARD_FLASH_BLOCK_SIZE := 262144
 
+#TARGET_PREBUILT_KERNEL := device/panasonic/ponyo/zImage
 TARGET_KERNEL_SOURCE := kernel/panasonic/ponyo_kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_ponyo_defconfig
 
