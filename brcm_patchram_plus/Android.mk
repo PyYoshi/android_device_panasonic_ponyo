3b76262c32ad5e795713bbe1fca7fc6a735ba51c
devrenax <cm4is01_devrenax@yahoo.co.jp>
2012-10-28 14:59:32 +0900

    brcm_patchram_plus: changed build flag

--- brcm_patchram_plus/Android.mk
+++ brcm_patchram_plus/Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(BOARD_HAVE_BLUETOOTH_BCM),false)
 LOCAL_PATH:= $(call my-dir)
 
 #
@@ -7,14 +8,12 @@ LOCAL_PATH:= $(call my-dir)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := brcm_patchram_plus.c
-LOCAL_MODULE := brcm_patchram_plus.dat
+LOCAL_MODULE := brcm_patchram_plus
 LOCAL_SHARED_LIBRARIES := libcutils
 LOCAL_MODULE_TAGS := optional
 
-#Add by Taoyuan for BCM4330 bluetooth address 2011.5.3
-ifeq ($(BOARD_HAVE_BLUETOOTH_BCM4330_BTADDR),true)
-  LOCAL_CFLAGS += -DNEED_BTADDR
-endif
+#LOCAL_CFLAGS += -DNEED_BTADDR
 
 include $(BUILD_EXECUTABLE)
 
+endif
