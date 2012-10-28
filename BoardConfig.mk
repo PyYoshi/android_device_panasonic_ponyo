886ff286b4e1eed82a3774f3c45187d35d8dc7a0
devrenax <cm4is01_devrenax@yahoo.co.jp>
2012-10-28 10:26:03 +0900

    コード整理

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,10 +9,10 @@ USE_CAMERA_STUB := true
 #TARGET_BOOTLOADER_BOARD_NAME := ponyo
 TARGET_OTA_ASSERT_DEVICE := p01d,ponyo,pana2_1
 
+# SoC defines and Build Flags
 TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := msm7x30
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
-
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -22,34 +22,32 @@ ARCH_ARM_HAVE_VFP := true
 #TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 #TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
-
+# Audio
 #BOARD_USES_GENERIC_AUDIO := false
 
+# Bluetooth
 BOARD_HAVE_BLUETOOTH := true
-# CM/system/bluetooth/brcm_patchram_plusを使用しないで、CM/device/panasonic/ponyo/brcm_patchram_plusを使うのでtrueにはしないこと
-BOARD_HAVE_BLUETOOTH_BCM := false
+# P-01Dのソースコードについてるbrcm_patch...を使う場合はfalseにすること。(現状ライブラリ依存関係で使いないけれど)
+BOARD_HAVE_BLUETOOTH_BCM := true
 #TARGET_NEEDS_BLUETOOTH_INIT_DELAY := true
+#BOARD_CUSTOM_BLUEDROID := ../../../device/panasonic/ponyo/bluedroid/bluetooth.c
 
+# Qcom Hardware defines and GPS defines
 BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QCOM_GPS := true
 BOARD_USES_QCOM_LIBS := true
 BOARD_USES_QCOM_LIBRPC := true
 BOARD_USE_QCOM_PMEM := true
-
-TARGET_PROVIDES_LIBRIL := true
-
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
 BOARD_VENDOR_QCOM_AMSS_VERSION := 50000
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := qcom
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
+# RIL
+TARGET_PROVIDES_LIBRIL := true
+
+# Graphics
 BOARD_EGL_CFG := device/panasonic/ponyo/prebuilt/egl.cfg
-
-BOARD_USE_SCREENCAP := true
-BOARD_OVERLAY_MINIFICATION_LIMIT := 2
-
-BOARD_USE_LEGACY_TOUCHSCREEN := true
-
 #BOARD_USE_FRAMEBUFFER_ALPHA_CHANNEL := true
 #BOARD_OVERLAY_FORMAT_YCbCr_420_SP := true
 USE_OPENGL_RENDERER := true
@@ -60,28 +58,24 @@ TARGET_USES_OVERLAY := true
 TARGET_GRALLOC_USES_ASHMEM := true
 TARGET_USES_GENLOCK := true
 TARGET_FORCE_CPU_UPLOAD := true
+BOARD_OVERLAY_MINIFICATION_LIMIT := 2
 
 #
+BOARD_USE_SCREENCAP := true
+
+# Touch screen
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+
+# USB Mass Storage
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk0p1
+
+# Recovery
 BOARD_CUSTOM_GRAPHICS := ../../../device/panasonic/ponyo/recovery/graphics.c
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/panasonic/ponyo/recovery/recovery_keys.c
 RECOVERY_RGBX := true
 
-#
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk0p1
-
-#sensors
+# Sensors
 #TARGET_USES_OLD_LIBSENSORS_HAL := true
-
-# Wifi related defines
-#BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-#WPA_SUPPLICANT_VERSION      := VER_0_6_X
-#BOARD_WLAN_DEVICE           := bcm4330
-#WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
-#WIFI_DRIVER_FW_PATH_STA     := "/etc/firmware/wlan/sdio_cit.bin"
-#WIFI_DRIVER_FW_PATH_AP      := "/etc/firmware/wlan/sdio_apsta.bin"
-#WIFI_DRIVER_FW_PATH_P2P     := "/vendor/firmware/fw_bcmdhd_p2p.bin"
-#WIFI_DRIVER_MODULE_ARG      := "firmware_path=/etc/firmware/wlan/sdio_cit.bin nvram_path=/etc/firmware/wlan/nvram.txt"
-#WIFI_DRIVER_MODULE_NAME     := "dhd"
 
 # Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
@@ -91,9 +85,10 @@ BOARD_WLAN_DEVICE := bcm4330
 WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
 WIFI_DRIVER_FW_PATH_STA := "/etc/firmware/wlan/sdio_cit.bin"
 WIFI_DRIVER_FW_PATH_AP := "/etc/firmware/wlan/sdio_apsta.bin"
-WIFI_DRIVER_MODULE_ARG      := "firmware_path=/etc/firmware/wlan/sdio_cit.bin nvram_path=/etc/firmware/wlan/nvram.txt"
-WIFI_DRIVER_MODULE_NAME     := "dhd"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/etc/firmware/wlan/sdio_cit.bin nvram_path=/etc/firmware/wlan/nvram.txt"
+WIFI_DRIVER_MODULE_NAME := "dhd"
 
+# Kernel Boot defines
 BOARD_KERNEL_CMDLINE := console=ttyMSM1 androidboot.hardware=qcom
 BOARD_KERNEL_BASE := 0x13800000
 BOARD_KERNEL_PAGESIZE := 4096
@@ -130,10 +125,12 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 # HEX(0x00040000) = DEC(262144)
 BOARD_FLASH_BLOCK_SIZE := 262144
 
+# Building Kernel
 #TARGET_PREBUILT_KERNEL := device/panasonic/ponyo/zImage
 TARGET_KERNEL_SOURCE := kernel/panasonic/ponyo_kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_ponyo_defconfig
 
+# Building WiFi Kernel Module
 KERNEL_EXTERNAL_MODULES:
 	make -C device/panasonic/ponyo/bcm4330/dhd/linux/ LINUXDIR=$(KERNEL_OUT) ARCH="arm" CROSS_COMPILE="arm-eabi-" dhd-cdc-sdmmc-gpl-debug
 	mv device/panasonic/ponyo/bcm4330/dhd/linux/dhd-cdc-sdmmc-gpl-debug-2.6.35.11/dhd.ko $(KERNEL_MODULES_OUT)
