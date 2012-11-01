8847ffe1780a978651e7fcf65ba275598b0519be
devrenax <cm4is01_devrenax@yahoo.co.jp>
2012-11-01 09:55:06 +0900

    remove checking proccesing

--- run_patch.sh
+++ run_patch.sh
@@ -4,13 +4,8 @@ echo "Patching Ponyo Workspace..."
 echo ""
 for p in $(find device/panasonic/ponyo/patches/ -name "*.diff") 
 	do 
-		echo -n "Apply patch "$(basename $p | awk -F"." '{print $1}')
+		echo -n "Apply patch "$(basename $p')
 		patch -p1 < $p > /dev/null 2>&1
-		if [ $? == 0 ]; then
-			echo "     [DONE]"
-		else
-			echo "     [FAIL]"
-		fi
 		echo "" 
 	done
 echo ""
