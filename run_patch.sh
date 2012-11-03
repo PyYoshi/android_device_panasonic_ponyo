5e2e65b6b00d8042be7dab26d10671f192ce0f70
devrenax <cm4is01_devrenax@yahoo.co.jp>
2012-11-03 17:46:58 +0900

    fixed syntax error

--- run_patch.sh
+++ run_patch.sh
@@ -4,7 +4,7 @@ echo "Patching Ponyo Workspace..."
 echo ""
 for p in $(find device/panasonic/ponyo/patches/ -name "*.diff") 
 	do 
-		echo -n "Apply patch "$(basename $p')
+		echo -n "Apply patch "$(basename $p)
 		patch -p1 < $p > /dev/null 2>&1
 		echo "" 
 	done
