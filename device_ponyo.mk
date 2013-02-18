7446686c6f25961cc01ab613dd49760c31597ba0
devrenax <cm4is01_devrenax@yahoo.co.jp>
2013-02-18 14:21:20 +0900

    add comments

--- device_ponyo.mk
+++ device_ponyo.mk
@@ -14,6 +14,13 @@ $(call inherit-product, build/target/product/full.mk)
 # proprietary side of the device
 $(call inherit-product-if-exists, device/panasonic/ponyo/PonyoProprietary.mk)
 
+# P-01D
+#ro.build.description=P_01D-user 2.3.4 GRH78 20120627.192518 release-keys
+#ro.build.fingerprint=docomo/P_01D/pana2_1:2.3.4/GRH78/20120627.192518:user/release-keys
+# SO-03D
+#ro.build.description=SO-03D-user 4.0.4 6.1.F.0.106 lb9-zw test-keys
+#ro.build.fingerprint=docomo/SO-03D_1256-7200/SO-03D:4.0.4/6.1.F.0.106/lb9-zw:user/release-keys
+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=P_01D BUILD_FINGERPRINT=docomo/P_01D/pana2_1:2.3.4/GRH78/20120627.192518:user/release-keys PRIVATE_BUILD_DESC="P_01D-user 2.3.4 GRH78 20120627.192518 release-keys"
 
 # high-density artwork where available
