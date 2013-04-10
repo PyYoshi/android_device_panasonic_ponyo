816fac5956594f333a8a94c2b1cf9eceb81d7a42
devrenax <cm4is01_devrenax@yahoo.co.jp>
2013-04-10 22:28:01 +0900

    enable to build liblights

--- Android.mk
+++ Android.mk
@@ -15,5 +15,6 @@
 LOCAL_PATH := $(my-dir)
 subdir_makefiles := \
 	$(LOCAL_PATH)/brcm_patchram_plus/Android.mk \
-	$(LOCAL_PATH)/hwaddrs/Android.mk
+	$(LOCAL_PATH)/hwaddrs/Android.mk \
+	$(LOCAL_PATH)/liblights/Android.mk
 include $(subdir_makefiles)
